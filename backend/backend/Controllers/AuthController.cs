3d4be1982dd9b3c0e606a26940e9c22d1dfddd16
André Costa <andree.costa@gmail.com>
2015-06-05 16:14:33 +0100

    base project, 2 apis
    base project, 2 apis

---
+++ backend/backend/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Facebook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace backend.Controllers
+{
+    public class AuthController : ApiController
+    {
+        [HttpGet]
+        public bool SetAuth(string id)
+        {
+            /*Set cookie*/
+            return true;
+        }
+
+        [HttpGet]
+        public string GetName(string id)
+        {
+            FacebookClient fbClient = new FacebookClient(id);
+            dynamic me = fbClient.Get("me");
+            return me.name;
+        }
+    }
+}
